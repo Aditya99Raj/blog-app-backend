e2b5f11444e015f24e15c81e0a4cf113a8736cba
AdityRaj99 <adityaraj9983@gmail.com>
2022-06-07 23:36:50 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connect = require("./configs/db")
 const app = express();
 app.use(express.json());
+const cors = require("cors");
 
 const blogController =require("./controllers/blog.conroller")
 const authController = require("./controllers/user.controller")
@@ -9,8 +10,10 @@ const authController = require("./controllers/user.controller")
 
 app.use("/api",blogController);
 app.use("/api",authController);
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => res.send("Working Fine"));
 
-app.listen(3004,async()=>{
+app.listen(port,async()=>{
     await connect();
     console.log("listening at port 3004");
 })
